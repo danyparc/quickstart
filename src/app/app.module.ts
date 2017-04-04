310e836465df5c3d5d6971d51fc5ff30d058a47e
Daniel Garcia <danyparc@gmail.com>
2017-04-04 16:30:11 -0500

    Tutorial: 6. Routing finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
-import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent }  from './heroes.component';
@@ -9,32 +8,20 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService }         from './hero.service';
 
+import { AppRoutingModule }   from './app-routing.module';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
   bootstrap:    [ AppComponent ],
   providers: [HeroService]
 
